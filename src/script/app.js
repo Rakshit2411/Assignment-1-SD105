3d3f6f49ddc68b67e2459a0f02e726c2f8951071
Rakshit Patel <Patel26@student.mitt.ca>
2020-12-18 18:21:42 -0600

    Getting elements from index file and API key

---
+++ src/script/app.js
@@ -0,0 +1,6 @@
+const APIKey = 'zpOgDArFTQol_ErrEKUk';
+const form = document.querySelector('form');
+const streetsElement = document.querySelector('.streets');
+const tableBody = document.querySelector('tbody');
+const streetDisplayTitle = document.getElementById('street-name');
+
