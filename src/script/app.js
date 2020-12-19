776c08212162d722ef91f82aadb581ccdf9addd1
Rakshit Patel <Patel26@student.mitt.ca>
2020-12-18 18:26:17 -0600

    Making functions to get the streets and display them

--- src/script/app.js
+++ src/script/app.js
@@ -4,3 +4,23 @@ const streetsElement = document.querySelector('.streets');
 const tableBody = document.querySelector('tbody');
 const streetDisplayTitle = document.getElementById('street-name');
 
+const getStreets = (streetName) => {
+  fetch('https://api.winnipegtransit.com/v3/streets.json?name=' + streetName + '&usage=long&api-key=' + APIKey)
+    .then(response => response.json())
+    .then(data => {
+      if (data.streets.length > 0) {
+        displayStreets(data.streets);
+      } else {
+        streetsElement.innerHTML = '<div class="no-results">No Streets found</div>';
+      }
+    });
+};
+
+const displayStreets = (streets) => {
+  streetsElement.textContent = '';
+  streets.forEach(street => {
+    streetsElement.insertAdjacentHTML('beforeend', `
+      <a href="#" data-street-key="${street.key}">${street.name}</a>
+    `);
+  });
+};
